91c65170c5f4e43392d5b6fb41de592c6e7dd7d5
Cristina ❤️ <mim0xina@gmail.com>
2026-03-03 11:55:22 +0100

    Modificación de la clase ServidorAdivina.java para que acepte más de un cliente en hilos diferentes (TODO)

--- src/main/java/com/actividad41/ServidorAdivina.java
+++ src/main/java/com/actividad41/ServidorAdivina.java
@@ -11,47 +11,11 @@ public class ServidorAdivina {
             ServerSocket socketServidor = new ServerSocket(2000);
             System.out.println("Esperando a que alguien se conecte para jugar...");
 
-            Socket miCliente = socketServidor.accept();
-
-            // Generamos el número secreto aquí para que no cambie durante la partida
-            int secreto = new Random().nextInt(101);
-            System.out.println("El número que tienen que adivinar es: " + secreto);
-
-            // Canales para leer y escribir datos
-            DataInputStream flujoEntrada = new DataInputStream(miCliente.getInputStream());
-            DataOutputStream flujoSalida = new DataOutputStream(miCliente.getOutputStream());
-
-            int numeroRecibido = -1;
-            final int MAX_INTENTOS = 5;
-            int intentos = 0;
-            boolean adivinado = false;
-
-            // El bucle sigue hasta que el cliente acierte o se cumplan los intentos
-            while (!adivinado && intentos < MAX_INTENTOS) {
-                numeroRecibido = flujoEntrada.readInt();
-                intentos ++;
-
-
-                if (numeroRecibido < secreto) {
-                    flujoSalida.writeUTF("Te has quedado corto, el número es mayor. LLevas " + intentos + " Intentos");
-                } else if (numeroRecibido > secreto) {
-                    flujoSalida.writeUTF("Te has pasado, el número es menor. LLevas "+ intentos + " Intentos");
-
-                } else {
-                    flujoSalida.writeUTF("Ese era el número!!!! Número encontrado en "+ intentos + " intentos");
-                    adivinado = true;
-                }
+            while(true){ //El servidor nunca se apaga
+                Socket miCLiente = socketServidor.accept(); // Espera clientes
+                System.out.println("Se ha conectado un cliente");
+                new HiloAdivina(miCLiente).start(); // cada cliente está en un hilo diferente
             }
-
-            // Solo se envía si NO adivinó
-            if (!adivinado) {
-                flujoSalida.writeUTF("Has llegado al máximo de intentos. El número era: " + secreto);
-            }
-
-            // Cerramos al terminar
-            miCliente.close();
-            socketServidor.close();
-
         } catch (Exception e) {
             System.out.println("Ha habido un fallo en el servidor: " + e.getMessage());
         }
